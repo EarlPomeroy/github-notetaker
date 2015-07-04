83684060cbe3314fa2e7f1af613e169790c7eb55
Earl Pomeroy <earl.pomeroy42@gmail.com>
2015-07-04 00:00:13 -0600

    Initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+class Home {
+    render() {
+        return (
+            <h2 className="text-center">
+                Search by Github Username Above
+            </h2>
+        )
+    }
+}
+
+export default Home;
